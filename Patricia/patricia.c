cbf0f53842dc8cb64f917befc8f353b6f0fc2e7b
Pawan Bhardwaj <pawan300891@gmail.com>
2015-08-21 16:27:56 +0530

    Fixed patricia bugs

--- Patricia/patricia.c
+++ Patricia/patricia.c
@@ -9,12 +9,15 @@ struct pnode {
 };
 static struct pnode *header;
 
-int find_firstbit_diffrence(x,y)
+int find_firstbit_diffrence(int x, int y)
 {
-    int temp = x ^ y;
-    int pos=0;
-    while(1<<pos & temp) pos++;
-
+    int temp,pos;
+    temp = x ^ y;
+    pos=0;
+    while(!(1<<pos & temp))
+    {
+        pos++;
+    }
     return pos;
 }
 
@@ -23,7 +26,7 @@ struct pnode * newnode(int val)
     struct pnode *p;
     p = (struct pnode *) malloc(sizeof(struct pnode));
     p->value = val;
-    p->bit   = 0;
+    p->bit   = -1;
     p->left = p->right = p;
 
     return p;
@@ -39,22 +42,32 @@ void insert(int x)
         header->right = header;
         return;
     }
+    //second node
+    if(header ->left == header)
+    {
+        header->left = p;
+        p->bit = 0;
+        if(x & 1 ) {
+            p->left = header;
+        } else {
+            p->right = header;
+        }
+        return;
+    }
 
     //search for node and find bit position
     struct pnode *pt = header->left;
-    prev_bit = 0;
+    prev_bit = -1;
     while(1)
     {    
         //find the diffrence in bit pos for direction
         dir  = x & (1<<pt->bit);
         //cache the prev bit before moving ahead
         prev_bit = pt->bit;
-
         if(dir == 0)
             pt = pt->left;
         else
             pt = pt->right;
-
         if(prev_bit >= pt->bit)
         {
             if(pt->value == x)
@@ -66,9 +79,9 @@ void insert(int x)
             break;
         }
     }
-    
     p->bit = find_firstbit_diffrence(pt->value , x);
-    
+    pt = header->left;
+    prev_bit = -1;
     while(1)
     {
         //find the diffrence in bit pos for direction
@@ -77,18 +90,18 @@ void insert(int x)
         prev_bit = pt->bit;
 
         if(dir == 0)
-        {   if(pt->left->bit >= p->bit)
+        {   if(pt->left->bit > p->bit)
                 break;
             pt = pt->left;
         }
         else
         {
-            if(pt->right->bit >= p->bit)
+            if(pt->right->bit > p->bit)
                 break;
             pt = pt->right;
         }
 
-        if(prev_bit == pt->bit)
+        if(prev_bit >= pt->bit)
             break;
     }
     
@@ -110,13 +123,19 @@ void insert(int x)
     
 }
 
-void main()
+int main()
 {
+    printf("\nInserting %d\n",10);
     insert(10);
-    insert(30);
-    insert(60);
-    insert(20);
-    insert(30);
+    printf("\nInserting %d\n",3);
+    insert(3);
+    printf("\nInserting %d\n",6);
+    insert(6);
+    printf("\nInserting %d\n",2);
+    insert(2);
+    printf("\nInserting %d\n",3);
+    insert(3);
+    return 0;
 }
 
     
